23db00067ec33e7330ab90f32650baa4fe8451a8
skyouo0727 <anwugame@gmail.com>
2022-08-22 22:23:44 +0800

    Init.

---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://papermc.io/repo/repository/maven-public/")
+    }
+}
+
+rootProject.name = "brilliant"
+
+include("brilliant-api", "brilliant-server")
